aa8c78d076548fa7962343117845f896a573c11b
wenqiangY <565435319@qq.com>
2018-11-26 10:08:52 +0800

    '新建login.vue组件'

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      path: '/login',
+      component: Login
     }
   ]
 })
